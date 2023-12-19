1cada24db0449d7c3a86d17632ee12273d5344a0
projtest23 <legolasgreenleaf424@gmail.com>
2023-12-19 21:10:30 +0330

    made project

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import CheckLogin  from '@/components/CheckCookies'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'dYdX',
+  description: 'dydx project',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="body"><CheckLogin/>
+      {children}</body>
+    </html>
+  )
+}
